64d42867c952298b3d0c807dd1a94fd707981563
Alex Frost <alexfrost4u@gmail.com>
2019-11-18 23:22:30 +0500

    Another change

--- src/Authorization.java
+++ src/Authorization.java
@@ -2,18 +2,21 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
+import javax.swing.*;
 
 public class Authorization {
 
     public static void main(String[] args) {
-
+        setupUI();
+    }
+    public static void setupUI(){
         // Creating frame
         JFrame mainPageFrame = new JFrame("Authorization");
         mainPageFrame.setSize(600, 400);
         mainPageFrame.setLocation(300,200);
+
+        JPanel panel = new JPanel();
+
         final JButton buttonAdmin = new JButton("Admin");
         mainPageFrame.getContentPane().add(BorderLayout.CENTER, buttonAdmin);
         mainPageFrame.setVisible(true);
