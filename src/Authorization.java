310991c1089b8cd62a7e38d2754d6c5ee3904015
Alex Frost <alexfrost4u@gmail.com>
2019-11-19 16:00:08 +0500

    Fix git problem

--- src/Authorization.java
+++ src/Authorization.java
@@ -2,9 +2,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-import javax.swing.border.Border;
 
-import javax.swing.*;
+
 
 public class Authorization {
 
--- src/User.java
+++ src/User.java
@@ -1,3 +1,8 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
 public class User {
 
     public static void main(String[] args) {
