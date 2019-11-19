ab71ab382e1d5f11efebed788a834417e38fb95d
zafarivaev <z.ivaev@mail.ru>
2019-11-19 22:53:35 +0500

    LogInViewController create

--- src/app/Controller/LogInViewController.java
+++ src/app/Controller/LogInViewController.java
@@ -1,31 +1,17 @@
 package app.Controller;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.*;
-
-public class LogInViewController {
+public class LogInViewController extends ViewController {
 
     public static void main(String[] args) {
         setupUI();
     }
 
     public static void setupUI(){
-        // Creating frame
-        mainPageFrame = new JFrame("app.Controller.Authorization");
-
-        JPanel panel = new JPanel();
-
-        final JButton buttonAdmin = new JButton("Admin");
-        mainPageFrame.getContentPane().add(BorderLayout.CENTER, buttonAdmin);
-        mainPageFrame.setVisible(true);
+        window.setTitle("Log In");
+        window.setVisible(true);
     }
 
     //Properties
-    static JFrame mainPageFrame;
-
 
 
 }
