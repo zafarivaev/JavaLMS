59f8f7f69e72756b09b951a479cae2b4c47a324f
Barno Asrolkhujayeva <Barno1686@gmail.com>
2019-12-22 16:16:03 +0500

    minor change in admin

--- src/app/Model/Admin.java
+++ src/app/Model/Admin.java
@@ -19,31 +19,13 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable(tableName = "admins")
-public class Admin {
+public class Admin extends User {
 
-    @DatabaseField(id = true)
-    private String name;
-    @DatabaseField
-    private String password;
-
-    public Admin() {
-        // ORMLite needs a no-arg constructor
-    }
-    public Admin(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
+    public Admin(String firstName,
+                     String lastName,
+                     Gender gender,
+                     String email,
+                     String password) {
+        super(firstName, lastName, gender, email, password, UserRole.Admin);
     }
 }
